b8219d783951e0dae1f266aec115f858ed55fa8f
Infortech <thiagotech66@gmail.com>
2026-06-08 13:32:01 -0300

    chore: inicializacao do projeto via spring initializr

---
+++ src/test/java/com/sistema/atendimento/AtendimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.atendimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtendimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
